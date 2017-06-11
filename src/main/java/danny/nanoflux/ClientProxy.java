42173b893c7730e84ec046f557681028bc71e3a5
Danny200287 <danny.mueller87@web.de>
2017-06-11 16:39:28 +0200

    initial import

---
+++ src/main/java/danny/nanoflux/ClientProxy.java
@@ -0,0 +1,24 @@
+package danny.nanoflux;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public final class ClientProxy extends CommonProxy {
+
+	@Override
+	public void preInit(FMLPreInitializationEvent e) {
+		super.preInit(e);
+	}
+
+	@Override
+	public void init(FMLInitializationEvent e) {
+		super.init(e);
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent e) {
+		super.postInit(e);
+	}
+
+}
---
+++ src/main/java/danny/nanoflux/Main.java
@@ -0,0 +1,37 @@
+package danny.nanoflux;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION, useMetadata = false)
+public final class Main {
+
+	public static final String MODID = "nanoflux";
+    public static final String VERSION = "1.0";
+    
+    @Instance
+    public static Main INSTANCE;
+    
+    @SidedProxy(clientSide="danny.nanoflux.ClientProxy", serverSide="danny.nanoflux.ServerProxy")
+    public static CommonProxy proxy;
+ 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+    	proxy.preInit(e);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+    	proxy.init(e);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+    	proxy.postInit(e);
+    }
+}
